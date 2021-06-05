3f647bb3f3323b4c361f833f3f3a83915b1e98aa
Wanderson.Macedo <fdisk.w@gmail.com>
2021-06-05 18:54:04 -0300

    repository dos com querys funcionando

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,9 +1,18 @@
 from ..config.database import Database
+from ..model.repository import Repository as RepositoryModel
 
 
 class Repository(Database):
 
-    def get_repository_by_userid(userid:int):
-        users_repo = self.cursor.execute("SELECT * FROM repository WHERE user_id = %s",(userid))
-        return users_repo
+    def get_repository_by_userid(self, user):
+        super().query.execute("SELECT * FROM repository WHERE user_id = %s", ([user[0]]))
+        return super().query.all()
 
+    def save(self, repository: RepositoryModel):
+        super().query.execute(
+            "INSERT INTO repository (name, url, access_type, size, stars, watchers, created_at, updated_at, user_id) "
+            "VALUES "
+            "(%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            ([repository.name, repository.url, repository.access_type, repository.size, repository.stars, repository.watchers, repository.created_at, repository.updated_at, repository.user_id])
+        )
+        return super().connect.commit()
--- app/repository/user.py
+++ app/repository/user.py
@@ -1,11 +1,24 @@
 from ..config.database import Database
+from ..model.user import User as UserModel
 
 
 class User(Database):
 
+    def save(self, user: str):
+        if not user:
+            return False
 
-    def getUser(self):
-        pass
+        super().query.execute("""INSERT INTO users (username) VALUES (%s)""", ([user]))
+        return super().connect.commit()
 
-    def save(self):
-        pass
+    def get_all(self):
+        super().query.execute('SELECT * FROM users')
+        return super().query.fetchall()
+
+    def get_by_id(self, user_id):
+        super().query.execute('SELECT * FROM users WHERE id = %s', ([user_id]))
+        return super().query.fetchone()
+
+    def get_by_username(self, username):
+        super().query.execute('SELECT * FROM users WHERE username = %s', ([username]))
+        return super().query.fetchone()
