2d40d75d3a5d20761ff282faabc1fddeabd72b03
Wanderson.Macedo <fdisk.w@gmail.com>
2021-06-05 18:46:57 -0300

    atualizacao no helper

--- app/utils/request_helper.py
+++ app/utils/request_helper.py
@@ -3,11 +3,14 @@ import httpx
 import os
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv()
+
 
 class RequestHelper:
+
     github_url = os.getenv("GITHUB_ENDPOINT")
 
-    async def getRequest(requested_endpoint: str):
+    async def get_request( requested_endpoint: str):
         full_url = RequestHelper.github_url + requested_endpoint
         async with httpx.AsyncClient() as client:
             resp: httpx.Response = await client.get(full_url, headers=RequestHelper.get_headers())
