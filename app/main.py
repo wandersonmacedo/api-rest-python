feeb77289ef7c4158e95611f4a40e4a583b05816
Wanderson.Macedo <fdisk.w@gmail.com>
2021-06-05 18:54:43 -0300

    arquivo main ainda funcionando como controller

--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ from typing import Optional
 from .service.git import Git
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
 @app.get("/repositories/{username}")
 async def get_repositories_by_username(username: str, from_local: Optional[bool] = False):
-    return await Git.get_user_repos(username,from_local)
+    git = Git()
+    return await git.get_user_repos(username, from_local)
 
 
 @app.get("/repositories/{repository_name}")
 async def get_repository_by_name(repository_name: str, save_data: Optional[bool] = False):
-    return await Git.get_repository_by_name(repository_name, save_data)
+    git = Git()
+    return await git.get_repository_by_name(repository_name, save_data)
